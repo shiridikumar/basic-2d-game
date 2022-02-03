b907a8e301bd62eb08a41d3e7c5c2eca9af5fd14
shiridikumar <p.shiridi@research.iiit.ac.in>
2022-02-04 02:00:50 +0530

    text rendering pending

--- src/game.cpp
+++ src/game.cpp
@@ -22,9 +22,9 @@ int player_total_score = 0;
 int coin_value = 10;
 vector<EnemyObject *> enemies;
 map<pair<float, float>, int> reserved;
-map<pair<float,float>,int> nextlevel;
+map<pair<float, float>, int> nextlevel;
 vector<GameObject *> coins;
-const glm::vec2 INITIAL_BALL_VELOCITY(0.0f, 350.0f);
+const glm::vec2 INITIAL_BALL_VELOCITY(0.0f, -250.0f);
 // Radius of the ball object
 const float BALL_RADIUS = 25.0f;
 
@@ -77,8 +77,8 @@ void Game::Init()
     this->num_walls = 5;
     reserved[make_pair(6, 0)] = 1;
     reserved[make_pair(7, 0)] = 1;
-    nextlevel[make_pair(6,0)]=1;
-    nextlevel[make_pair(7,0)]=1;
+    nextlevel[make_pair(6, 0)] = 1;
+    nextlevel[make_pair(7, 0)] = 1;
     for (int i = 0; i < this->num_walls; i++)
     {
         srand(time(NULL) + i);
@@ -113,7 +113,7 @@ void Game::Init()
             randomx = (rand() % (int)(12)) + 1;
             randomy = (rand() % (int)(13)) + 1;
         }
-        cout << randomx << " " << randomy << endl;
+        //cout << randomx << " " << randomy << endl;
         glm::vec2 coinPos = glm::vec2(randomx * coin_size.x, randomy * coin_size.x);
         coins.push_back(new GameObject(coinPos, coin_size, ResourceManager::GetTexture("coins")));
     }
@@ -125,7 +125,7 @@ void Game::Update(float dt)
 {
     for (int i = 0; i < this->num_enemies; i++)
     {
-        enemies[i]->Move(dt, this->Width, this->Height*(this->level_no),(this->Height)*(level_no-1),this->level_no);
+        enemies[i]->Move(dt, this->Width, this->Height * (this->level_no), (this->Height) * (level_no - 1), this->level_no);
     }
     for (int i = 0; i < coins.size(); i++)
     {
@@ -141,29 +141,53 @@ void Game::Update(float dt)
             break;
         }
     }
-    //cout<<coins.size()<<endl;
+    for (int i = 0; i < enemies.size(); i++)
+    {
+        if (abs(enemies[i]->Position.y - Player->Position.y) < 45.0f && abs(enemies[i]->Position.x - Player->Position.x) < 45.0f)
+        {
+            //cout<<player_level_score<<endl;
+            cout<<"GAME OVER"<<endl;
+            cout<<"Levels cleared : "<<this->level_no-1<<endl;
+            cout << "----------------------------------------------------" << endl;
+            cout << "Total score : " << player_level_score << endl;
+            exit(0);
+        }
+    }
+
+    ////cout<<coins.size()<<endl;
 }
 
 void Game::level2()
 {
     this->level_no++;
     int pad = this->Height * (level_no - 1);
-    cout << pad << endl;
+    //cout << pad << endl;
     reserved.clear();
     glm::vec2 playerPos = glm::vec2(this->Width / 2.0f, this->Height / 2.0f);
     float randomx = 0;
     float randomy = 0;
     this->num_walls = 5 * this->level_no;
-    this->num_enemies = 5 * this->level_no;
+    this->num_enemies = 4 * this->level_no;
     this->num_coins = 5 * this->level_no;
 
     GameLevel two;
     Levels.clear();
-    if(this->level_no==2){
+    if (this->level_no == 2)
+    {
         two.Load("../src/two.lvl", this->Width, this->Height + pad);
     }
-    else{
+    else if (this->level_no == 3)
+    {
+
         two.Load("../src/three.lvl", this->Width, this->Height + pad);
+    }
+    else
+    {
+        cout<<"Game over"<<endl;
+        cout<<"****Congratulations you have cleared all the levels*****"<<endl;
+        cout << "----------------------------------------------------" << endl;
+        cout << "Total score : " << player_level_score << endl;
+        exit(0);
     }
     this->Levels.push_back(two);
 
@@ -173,15 +197,15 @@ void Game::level2()
     enemies.clear();
     reserved[make_pair(6, 0 + (int)(pad / 50.0f))] = 1;
     reserved[make_pair(7, 0 + (int)(pad / 50.0f))] = 1;
-    nextlevel[make_pair(7,0+(int)(pad/50.0f))]=1;
-    nextlevel[make_pair(6,0+(int)(pad/50.0f))]=1;
-    
+    nextlevel[make_pair(7, 0 + (int)(pad / 50.0f))] = 1;
+    nextlevel[make_pair(6, 0 + (int)(pad / 50.0f))] = 1;
+    const glm::vec2 velocity = INITIAL_BALL_VELOCITY - glm::vec2(0.0f, this->level_no * 30);
 
     for (int i = 0; i < this->num_walls; i++)
     {
         srand(time(NULL) + i);
         float randomx = (rand() % (int)(12)) + 1;
-        float randomy = (rand() % (int)(12 -2+1)) + 2 + (int)(pad / 50.0f);
+        float randomy = (rand() % (int)(12 - 2 + 1)) + 2 + (int)(pad / 50.0f);
         reserved[make_pair(randomx, randomy)] = 1;
         obstacles.push_back(new GameObject(glm::vec2(randomx * (coin_size.x), randomy * coin_size.x), coin_size, ResourceManager::GetTexture("block")));
     }
@@ -193,7 +217,7 @@ void Game::level2()
         // Get a random number
         float randomx = (rand() % 12);
         float randomy = (rand() % 13) + (int)(pad / 50.0f);
-        enemies.push_back(new EnemyObject( glm::vec2(randomx * coin_size.x, randomy * coin_size.x), BALL_RADIUS, INITIAL_BALL_VELOCITY,
+        enemies.push_back(new EnemyObject(glm::vec2(randomx * coin_size.x, randomy * coin_size.x), BALL_RADIUS, velocity,
                                           ResourceManager::GetTexture("face")));
     }
     for (int i = 0; i < this->num_coins; i++)
@@ -261,17 +285,18 @@ void Game::ProcessInput(float dt)
         if (this->Keys[GLFW_KEY_W])
         {
             int flag = 1;
-            if(nextlevel[make_pair((int)Player->Position.x/50,(int)Player->Position.y/50)]==1){
+            if (nextlevel[make_pair((int)Player->Position.x / 50, (int)Player->Position.y / 50)] == 1)
+            {
                 glm::mat4 view = glm::mat4(1.0f);
-                pos = glm::vec3(0.0f, this->Height*(this->level_no), 0.0f);
+                pos = glm::vec3(0.0f, this->Height * (this->level_no), 0.0f);
                 view = glm::lookAt(pos, pos + front, up);
                 ResourceManager::GetShader("sprite").SetMatrix4("view", view);
                 level2();
             }
-            
-            if (Player->Position.y > 50.0f+((this->level_no-1)*(this->Height)))
+
+            if (Player->Position.y > 50.0f + ((this->level_no - 1) * (this->Height)))
             {
-                
+
                 for (int i = 0; i < this->num_walls; i++)
                 {
                     if ((abs(obstacles[i]->Position.x - Player->Position.x) < 45.0f) && ((Player->Position.y - obstacles[i]->Position.y) < 50.0f) && Player->Position.y - obstacles[i]->Position.y > 0.0f)
@@ -282,7 +307,7 @@ void Game::ProcessInput(float dt)
                 }
                 if (flag == 1)
                 {
-                    //cout<<(int)(Player->Position.x/50.0f)<<" "<<(int)(Player->Position.y/50.0f)<<endl;
+                    ////cout<<(int)(Player->Position.x/50.0f)<<" "<<(int)(Player->Position.y/50.0f)<<endl;
                     Player->Position.y -= velocity;
                 }
             }
@@ -290,7 +315,7 @@ void Game::ProcessInput(float dt)
         if (this->Keys[GLFW_KEY_S])
         {
             int flag = 1;
-            if (Player->Position.y < this->Height*(this->level_no) + - 2 * 50.0f)
+            if (Player->Position.y < this->Height * (this->level_no) + -2 * 50.0f)
             {
                 for (int i = 0; i < this->num_walls; i++)
                 {
@@ -322,7 +347,7 @@ void Game::Render()
         for (int i = 0; i < this->num_enemies; i++)
         {
             // if(this->level_no==2){
-            //     cout<<enemies[i]->Position.y<<endl;
+            //     //cout<<enemies[i]->Position.y<<endl;
             // }
             enemies[i]->Draw(*Renderer);
         }
